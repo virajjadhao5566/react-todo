c05829e99f69deddb3e27e992d72baaf6ff6f54f
Viraj <virajjadhao85@gmail.com>
2023-03-29 09:48:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+  const [todoList,setTodoList] = useState([]);
+  const [currentTask,setCurrentTask] = useState("")
+
+  const addTask = () => {
+    setTodoList([...todoList,currentTask])
+  }
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <input type="text" placeholder='Enter Text'/>
-      <button>Submit</button>
+      <input 
+        type="text" 
+        placeholder='Enter Text'
+        onChange={(event)=>{
+          setCurrentTask(event.target.value)
+        }}
+      />
+      <button onClick={addTask}>Add Task</button>
       <hr/>
+      <ul>
+        {
+          todoList.map((val,key) =>{
+            return <li>{val}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
