b7a54886ed7eba6a989125ff88e90b6ecc7fe0bc
Viraj <virajjadhao85@gmail.com>
2023-04-02 11:00:06 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import './App.css';
-import React,{useState} from 'react'
+import React,{useState,useRef} from 'react'
 
 function App() {
   const [todoList,setTodoList] = useState([]);
   const [currentTask,setCurrentTask] = useState("")
-
+  const inputTask = useRef(null)
   const addTask = () => {
-    setTodoList([...todoList,currentTask])
+    setTodoList([...todoList,{task:currentTask,completed:false}])
+    inputTask.current.value = ""
+    setCurrentTask("")
+  }
+  const deleteTask = (toDelete) =>{
+    setTodoList(
+      todoList.filter((task) =>{
+        return task.task !== toDelete
+      })
+    )
+  }
+  const completeTask = (toComplete) =>{
+    setTodoList(
+      todoList.map((task) =>{
+        return task.task === toComplete 
+        ? {task : toComplete, completed:true}
+        : {task : task.task, completed:task.completed? true : false}
+      })
+    )
   }
   return (
     <div className="App">
       <h1>To Do List</h1>
       <input 
+        ref={inputTask}
         type="text" 
         placeholder='Enter Text'
+        onKeyDown={(event)=>{
+          if(event.keyCode === 13){
+            addTask()
+          }
+        }}
         onChange={(event)=>{
           setCurrentTask(event.target.value)
         }}
@@ -23,7 +47,20 @@ function App() {
       <ul>
         {
           todoList.map((val,key) =>{
-            return <li>{val}</li>
+            return(
+              <div className='task'>
+                <li key = 'key'>{val.task}</li>
+                <button onClick={()=>
+                  completeTask(val.task)}
+                >Complete</button>
+                <button onClick={()=>
+                  deleteTask(val.task)}
+                >X</button>
+                {
+                  val.completed?(<h1>Task Complete</h1>):(<h1>Task Not Complete</h1>)
+                }
+              </div>
+            )
           })
         }
       </ul>
